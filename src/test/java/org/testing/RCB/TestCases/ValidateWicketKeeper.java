c66ede5af95b5af502d8e07769241cf0c6f183fe
sushilQA <101033577+sushilQA@users.noreply.github.com>
2023-03-19 11:23:42 +0530

    RCB Team

--- src/test/java/org/testing/RCB/TestCases/ValidateWicketKeeper.java
+++ src/test/java/org/testing/RCB/TestCases/ValidateWicketKeeper.java
@@ -18,5 +18,27 @@ public class ValidateWicketKeeper {
 
 	public void validateWicketKeeper(File file) throws IOException, BiffException {
 		System.out.println("******************** Validate Wicket Keeper ********************\n");
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			// Loading given json file into poojo
+			RCB rcb = mapper.readValue(file, RCB.class);
+			int wicketKeeper = 0;
+			if (rcb.getPlayer().size() == 11) {
+				for (Players p : rcb.getPlayer()) {
+					if (p.getRole().equalsIgnoreCase("Wicket-keeper")) {
+						wicketKeeper++;
+					}
+				}
+				if (wicketKeeper == 1) {
+					System.out.println("Submitted team is qualified for selection as the same have 1 wicket keeper");
+				} else
+					System.out.println("Submitted team is not qualified for selection as the same have " + wicketKeeper
+							+ " wicket keeper");
+			} else
+				System.out.println("Team size is not as per IPL standerd");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
