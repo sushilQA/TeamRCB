26ebf9c2496cc47bd0622c25ccd0329bf2aa579a
sushilQA <101033577+sushilQA@users.noreply.github.com>
2023-03-19 11:40:58 +0530

    To string getter setter

--- src/test/java/org/testing/RCB/Resources/Players.java
+++ src/test/java/org/testing/RCB/Resources/Players.java
@@ -4,41 +4,50 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Players{
-    public String name;
-    @Override
+public class Players {
+	public String name;
+
+	public String country;
+	public String role;
+	@JsonProperty("price-in-crores")
+	public String priceInCrores;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPriceInCrores() {
+		return priceInCrores;
+	}
+
+	public void setPriceInCrores(String priceInCrores) {
+		this.priceInCrores = priceInCrores;
+	}
+
+	@Override
 	public String toString() {
 		return "Players [name=" + name + ", country=" + country + ", role=" + role + ", priceInCrores=" + priceInCrores
 				+ "]";
 	}
-	public String country;
-    public String role;
-    @JsonProperty("price-in-crores")
-    public String priceInCrores;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getPriceInCrores() {
-		return priceInCrores;
-	}
-	public void setPriceInCrores(String priceInCrores) {
-		this.priceInCrores = priceInCrores;
-	}
-    
-    
+
 }
